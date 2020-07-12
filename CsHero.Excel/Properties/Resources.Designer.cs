6546873acd4d23f5786ee07918cdc531f12666b5
@cshero <has.cha.89@gmail.com>
2020-07-12 10:51:44 +0300

    first commit

--- CsHero.Excel/Properties/Resources.Designer.cs
+++ CsHero.Excel/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Gs.Request.Comparer.Properties {
+namespace CsHero.Excel.Comparer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Gs.Request.Comparer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gs.Request.Comparer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CsHero.Excel.Comparer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
